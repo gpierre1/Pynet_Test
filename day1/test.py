a9739b6b5a9392c49fb44b89e94a2056f6367957
John Doe <jdoe@domain.com>
2016-11-07 09:00:31 -0800

    day1

---
+++ day1/test.py
@@ -0,0 +1,12 @@
+name1 = "Don Trum"
+name2 = "Hill CLinton"
+name3 = "Geo Washington"
+name4 = raw_input("Enter fourth name: ")
+
+print
+print "{:>30}".format(name1)
+print "{:>30}".format(name2)
+print "{:>30}".format(name3)
+print "{:>30}".format(name4)
+print
+
